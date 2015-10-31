f259484d264a81f7e00ba9551518bf90057242e1
gocreating <gocreating@gmail.com>
2015-11-01 04:11:04 +0800

    start developing cli

---
+++ docs/Development Guide.md
@@ -0,0 +1,13 @@
+# Development Guide
+
+This documentation will guide you to develop seed framework
+
+## Installation
+
+```
+$ npm install -g gulp
+$ npm install
+$ gulp
+```
+
+install packages and use gulp to watch and rebuild source files
---
+++ src/cli/index.js
@@ -0,0 +1,22 @@
+export function displayUsage() {
+  console.log(`
+    Usage: seed <command> [options]
+
+    Commands:
+      hello   say hello
+      help    display help message
+  `);
+}
+
+export function run() {
+  let userArgs = process.argv.slice(2);
+  let command = userArgs[0];
+
+  if (command === undefined) {
+    displayUsage();
+  } else if (command === 'hello') {
+    console.log('hello world');
+  } else if (command === 'help') {
+    displayUsage();
+  }
+}
