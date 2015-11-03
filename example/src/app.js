48a727b9470ffeada6f0d31ea01e35bb595c39d2
gocreating <gocreating@gmail.com>
2015-11-03 17:56:11 +0800

    add orm

--- example/src/app.js
+++ example/src/app.js
@@ -1,5 +1,54 @@
 import * as exseed from 'exseed';
+import Waterline from 'waterline';
+import waterlineSqlite3 from 'waterline-sqlite3';
 import http from 'http';
+
+const config = {
+  adapters: {
+    'sqlite3': waterlineSqlite3
+  },
+  connections: {
+    default: {
+      adapter: 'sqlite3',
+      type: 'disk',
+      // base on cwd (current working directory)
+      filename: './db.development.sqlite',
+      debug: true, // show SQL queries or not
+    },
+  },
+};
+
+const User = Waterline.Collection.extend({
+  identity: 'user',
+  connection: 'default',
+  attributes: {
+    firstName: 'string',
+    lastName: 'string',
+  },
+});
+
+const waterline = new Waterline();
+waterline.loadCollection(User);
+waterline.initialize(config, (err, ontology) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  // Tease out fully initialised models.
+  const User = ontology.collections.user;
+
+  User
+    .create({
+      firstName: 'Neil',
+      lastName: 'Armstrong',
+    })
+    .then((user) => {
+      console.dir(user);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
 
 class App1 extends exseed.App {
   /**
